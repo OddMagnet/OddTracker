2e60fee3d07444e492ba7844b258e8318941f986
OddMagnet <mibruenen@gmail.com>
2020-11-15 16:07:41 +0100

    Initial Commit

---
+++ OddTracker/OddTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddTrackerApp.swift
+//  OddTracker
+//
+//  Created by Michael Brünen on 15.11.20.
+//
+
+import SwiftUI
+
+@main
+struct OddTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
