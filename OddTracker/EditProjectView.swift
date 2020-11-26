3fa2be7cb6164b1d285714de4582edd069c88d54
OddMagnet <mibruenen@gmail.com>
2020-11-26 16:18:08 +0100

    Finish EditProjectView, add NavigationLink to ProjectHeaderView

--- OddTracker/EditProjectView.swift
+++ OddTracker/EditProjectView.swift
@@ -14,10 +14,14 @@ struct EditProjectView: View {
     ]
 
     @EnvironmentObject var dataController: DataController
+    @Environment(\.presentationMode) var presentationMode
+
 
     @State private var title: String
     @State private var detail: String
     @State private var color: String
+    @State private var showingDeleteConfirm = false
+
 
     init(project: Project) {
         self.project = project
@@ -34,13 +38,60 @@ struct EditProjectView: View {
                 TextField("Description of this project", text: $title.onChange(update))
             }
 
-            
+            Section(header: Text("Custom project color")) {
+                LazyVGrid(columns: colorColumns) {
+                    ForEach(Project.colors, id: \.self) { item in
+                        ZStack {
+                            Color(item)
+                                .aspectRatio(1, contentMode: .fit)
+                                .cornerRadius(6)
+
+                            if item == color {
+                                Image(systemName: "checkmark.circle")
+                                    .foregroundColor(.white)
+                                    .font(.largeTitle)
+                            }
+                        }
+                        .onTapGesture {
+                            color = item
+                            update()
+                        }
+                    }
+                }
+                .padding(.vertical)
+            }
+
+            Section(footer: Text("Closing a project moves it from the Open to Closed tab; deleting it removes the project completely.")) {
+                Button(project.isClosed ? "Reopen this project" : "Close this project") {
+                    project.isClosed.toggle()
+                    update()
+                }
+
+                Button("Delete this project") {
+                    showingDeleteConfirm.toggle()
+                }
+                .accentColor(.red)
+            }
         }
         .navigationTitle("Edit Project")
+        .onDisappear(perform: dataController.save)
+        .alert(isPresented: $showingDeleteConfirm) {
+            Alert(title: Text("Delete project?"),
+                  message: Text("Are you sure you want to delete this project? You will also delete all the items it contains."),
+                  primaryButton: .default(Text("Delete"), action: delete),
+                  secondaryButton: .cancel())
+        }
     }
 
     func update() {
+        project.title = title
+        project.detail = detail
+        project.color = color
+    }
 
+    func delete() {
+        dataController.delete(project)
+        presentationMode.wrappedValue.dismiss()
     }
 }
 
