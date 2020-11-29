dcdddba0f67f8b7c5c20d10896b1e0248558cf8c
OddMagnet <mibruenen@gmail.com>
2020-11-29 14:07:56 +0100

    Show an Alert with details when the user taps an award, color earned awards

--- OddTracker/AwardsView.swift
+++ OddTracker/AwardsView.swift
@@ -11,6 +11,11 @@ struct AwardsView: View {
     // Tag for the TabView in `ContentView.swift`
     static let tag: String? = "Awards"
 
+    @EnvironmentObject var dataController: DataController
+
+    @State private var selectedAward = Award.example
+    @State private var showingAwardDetails = false
+
     var columns: [GridItem] {
         [GridItem(.adaptive(minimum: 100, maximum: 100))]
     }
@@ -21,19 +26,30 @@ struct AwardsView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(Award.allAwards) { award in
                         Button {
-                            // TODO: - Add button action
+                            selectedAward = award
+                            showingAwardDetails = true
                         } label: {
                             Image(systemName: award.image)
                                 .resizable()
                                 .scaledToFit()
                                 .padding()
                                 .frame(width: 100, height: 100)
-                                .foregroundColor(Color.secondary.opacity(0.5))
+                                .foregroundColor(dataController.hasEarned(award: award)
+                                                    ? Color(award.color)
+                                                    : Color.secondary.opacity(0.5)
+                                )
                         }
                     }
                 }
             }
             .navigationTitle("Awards")
+        }
+        .alert(isPresented: $showingAwardDetails) {
+            if dataController.hasEarned(award: selectedAward) {
+                return Alert(title: Text("Unlocked: \(selectedAward.name)"), message: Text("\(Text(selectedAward.description))"), dismissButton: .default(Text("OK")))
+            } else {
+                return Alert(title: Text("Locked"), message: Text("\(Text(selectedAward.description))"), dismissButton: .default(Text("OK")))
+            }
         }
     }
 }
