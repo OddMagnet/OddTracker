9137f5926aa2d840bf2f4da23b75f8b71953172d
OddMagnet <mibruenen@gmail.com>
2020-12-22 11:10:09 +0100

    Testing

---
+++ OddTracker/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  OddTracker
+//
+//  Created by Michael Brünen on 22.12.20.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
