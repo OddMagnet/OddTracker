c1b65efe5b280958e59dfee989443123596d0942
OddMagnet <mibruenen@gmail.com>
2021-01-27 17:01:00 +0100

    Add tests for the examples of Project and Item

--- OddTrackerTests/DevelopmentTests.swift
+++ OddTrackerTests/DevelopmentTests.swift
@@ -24,4 +24,14 @@ class DevelopmentTests: BaseTestCase {
         XCTAssertEqual(dataController.count(for: Project.fetchRequest()), 0, "There should be no sample projects after deleteAll()")
         XCTAssertEqual(dataController.count(for: Item.fetchRequest()), 0, "There should be no sample items after deleteAll()")
     }
+
+    func testExampleProjectIsClosed() {
+        let project = Project.example
+        XCTAssertTrue(project.isClosed, "The example project should be closed")
+    }
+
+    func testExampleItemIsHighPriority() {
+        let item = Item.example
+        XCTAssertEqual(item.priority, 3, "The examample item should be high (3) priority")
+    }
 }
