d9ac3696a55d0c1dde1d4c5dd2c5ba927c034822
OddMagnet <mibruenen@gmail.com>
2021-06-15 17:50:41 +0200

    Update the entry Struct, the Provider struct and its functions, as well as the Widgets UI

--- OddTrackerWidget/OddTrackerWidget.swift
+++ OddTrackerWidget/OddTrackerWidget.swift
@@ -11,40 +11,49 @@ import Intents
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+        SimpleEntry(date: Date(), items: [Item.example])
+    }
+
+    /// Load the top items
+    /// - Returns: An array of the top items
+    func loadItems() -> [Item] {
+        print("loading items")
+        let dataController = DataController()
+        let itemRequest = dataController.fetchRequestForTopItems(count: 1)
+        return dataController.results(for: itemRequest)
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> Void) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        let entry = SimpleEntry(date: Date(), items: loadItems())
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        let entry = SimpleEntry(date: Date(), items: loadItems())
+        let timeline = Timeline(entries: [entry], policy: .never)
         completion(timeline)
     }
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let configuration: ConfigurationIntent
+    let items: [Item]
 }
 
 struct OddTrackerWidgetEntryView: View {
     var entry: Provider.Entry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        VStack {
+            Text("Up next…")
+                .font(.title)
+
+            if let item = entry.items.first {
+                Text(item.itemTitle)
+            } else {
+                Text("Nothing!")
+            }
+        }
     }
 }
 
@@ -63,7 +72,7 @@ struct OddTrackerWidget: Widget {
 
 struct OddTrackerWidget_Previews: PreviewProvider {
     static var previews: some View {
-        OddTrackerWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+        OddTrackerWidgetEntryView(entry: SimpleEntry(date: Date(), items: [Item.example]))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
