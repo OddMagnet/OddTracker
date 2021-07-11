a60983e8703ae940b524900abf6f75bbf084eeb8
OddMagnet <mibruenen@gmail.com>
2021-07-11 16:31:48 +0200

    Fix bug stopping Widget from working properly

--- OddTrackerWidget/OddTrackerWidget.swift
+++ OddTrackerWidget/OddTrackerWidget.swift
@@ -9,7 +9,7 @@ import WidgetKit
 import SwiftUI
 import Intents
 
-struct Provider: IntentTimelineProvider {
+struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), items: [Item.example])
     }
@@ -17,18 +17,17 @@ struct Provider: IntentTimelineProvider {
     /// Load the top items
     /// - Returns: An array of the top items
     func loadItems() -> [Item] {
-        print("loading items")
         let dataController = DataController()
         let itemRequest = dataController.fetchRequestForTopItems(count: 1)
         return dataController.results(for: itemRequest)
     }
 
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> Void) {
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
         let entry = SimpleEntry(date: Date(), items: loadItems())
         completion(entry)
     }
 
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
         let entry = SimpleEntry(date: Date(), items: loadItems())
         let timeline = Timeline(entries: [entry], policy: .never)
         completion(timeline)
@@ -62,7 +61,7 @@ struct OddTrackerWidget: Widget {
     let kind: String = "OddTrackerWidget"
 
     var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
             OddTrackerWidgetEntryView(entry: entry)
         }
         .configurationDisplayName("My Widget")
