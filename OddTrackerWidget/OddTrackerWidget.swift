03ff09e73dd7741296c57b87bb029ccb1352daab
OddMagnet <mibruenen@gmail.com>
2021-12-28 12:33:37 +0100

    Finish new widget UI

--- OddTrackerWidget/OddTrackerWidget.swift
+++ OddTrackerWidget/OddTrackerWidget.swift
@@ -82,8 +82,58 @@ struct SimpleOddTrackerWidget: Widget {
 struct OddTrackerWidgetMultipleEntryView: View {
     var entry: Provider.Entry
 
+    @Environment(\.widgetFamily) var widgetFamily
+    @Environment(\.sizeCategory) var sizeCategory
+    var items: ArraySlice<Item> {
+        let itemCount: Int
+
+        switch widgetFamily {
+            case .systemSmall:
+                itemCount = 1
+            case .systemMedium:
+                if sizeCategory < .extraLarge {
+                    itemCount = 3
+                } else {
+                    itemCount = 2
+                }
+            case .systemLarge:
+                if sizeCategory < .extraExtraLarge {
+                    itemCount = 5
+                } else {
+                    itemCount = 4
+                }
+            default:
+                // For future widgetFamilies
+                itemCount = 2
+        }
+
+        return entry.items.prefix(itemCount)
+    }
+
     var body: some View {
-        Text("Hello, world!")
+        VStack(spacing: 5) {
+            ForEach(items) { item in
+                HStack {
+                    Color(item.project?.color ?? "Light Blue")
+                        .frame(width: 5)
+                        .clipShape(Capsule())
+
+                    VStack(alignment: .leading) {
+                        Text(item.itemTitle)
+                            .font(.headline)
+                            .layoutPriority(1)
+
+                        if let projectTitle = item.project?.projectTitle {
+                            Text(projectTitle)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+
+                    Spacer()
+                }
+            }
+        }
+        .padding(20)
     }
 }
 
