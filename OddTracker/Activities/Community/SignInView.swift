a41210e46d42080be883535dbf5dcfd8dd0c1c68
OddMagnet <mibruenen@gmail.com>
2021-12-29 18:09:26 +0100

    Add `configureSignIn()` and `completeSignIn` functions

--- OddTracker/Activities/Community/SignInView.swift
+++ OddTracker/Activities/Community/SignInView.swift
@@ -72,13 +72,51 @@ struct SignInView: View {
     /// Handles the configuration of a "Sign in with Apple" request
     /// - Parameter request: The "Sign in with Apple" request
     func configureSignIn(_ request: ASAuthorizationAppleIDRequest) {
-
+        request.requestedScopes = [.fullName]
     }
 
     /// Finishes a "Sign in with Apple" request
     /// - Parameter result: The result of the "Sign in with Apple" request
     func completeSignIn(_ result: Result<ASAuthorization, Error>) {
+        switch result {
+            case .success(let auth):
+                // check the given `ASAuthorization` instance for an Apple ID
+                if let appleID = auth.credential as? ASAuthorizationAppleIDCredential {
+                    // check that the fullName is present
+                    if let fullName = appleID.fullName {
+                        // prepare the username
+                        let formatter = PersonNameComponentsFormatter()
+                        var username = formatter.string(from: fullName).trimmingCharacters(in: .whitespacesAndNewlines)
 
+                        // if the username is empty, create a random one
+                        if username.isEmpty {
+                            username = "User-\(Int.random(in: 1001...9999))"
+                        }
+
+                        // save the username both locally in `UserDefaults` and in the cloud via `NSUbiquitousKeyValueStore`
+                        UserDefaults.standard.set(username, forKey: "username")
+                        NSUbiquitousKeyValueStore.default.set(username, forKey: "username")
+
+                        // set the status and close the view
+                        status = .authorized
+                        close()
+                        return
+                    }
+                }
+
+                // if something failed, set the status to failure
+                status = .failure(nil)
+            case .failure(let error):
+                if let error = error as? ASAuthorizationError {
+                    // if the user cancelled, then just set the status to .unknown
+                    if error.errorCode == ASAuthorizationError.canceled.rawValue {
+                        status = .unknown
+                        return
+                    }
+                }
+                // if apple refused to authenticate the user, then it's a hard failure
+                status = .failure(error)
+        }
     }
 }
 
