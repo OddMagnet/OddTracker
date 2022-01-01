caf6821f21afd250a6b67895fd59425df14c043c
OddMagnet <mibruenen@gmail.com>
2022-01-01 17:35:00 +0100

    Update UI for new functionality

--- OddTracker/Activities/Project/EditProjectView.swift
+++ OddTracker/Activities/Project/EditProjectView.swift
@@ -102,8 +102,17 @@ struct EditProjectView: View {
         }
         .navigationTitle("Edit Project")
         .toolbar {
-            Button(action: uploadToCloud) {
-                Label("Upload to iCloud", systemImage: "icloud.and.arrow.up")
+            switch cloudStatus {
+                case .checking:
+                    ProgressView()
+                case .exists:
+                    Button(action: removeFromCloud) {
+                        Label("Remove from iCloud", systemImage: "icloud.slash")
+                    }
+                case .absent:
+                    Button(action: uploadToCloud) {
+                        Label("Upload to iCloud", systemImage: "icloud.and.arrow.up")
+                    }
             }
         }
         .onDisappear(perform: dataController.save)
@@ -115,6 +124,7 @@ struct EditProjectView: View {
                 secondaryButton: .cancel()
             )
         }
+        .onAppear(perform: updateCloudStatus)
         .sheet(isPresented: $showingSignIn, content: SignInView.init)
     }
 
